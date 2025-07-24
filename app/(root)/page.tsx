26a674b37343cff46a80ba049e8c0da0e4b4bc6c
Henok Tilahun Lakew <henoktilahun363@gmail.com>
2025-07-24 10:45:30 +0300

    Mobile Nav and nav links

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth,  } from "@/auth";
+
 
 
 const Home = async () => {
@@ -9,12 +8,7 @@ const Home = async () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-      <form className="px-10 pt-[100px]" action={async () => {
-        "use server";
-        await signOut({ redirectTo: ROUTES.SIGN_IN})
-      }}>
-        <Button type="submit">Log Out</Button>
-      </form>
+     
     </>
   );
 }
